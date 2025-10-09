a8113cc11b9c18c65ec3fa7be7a57347f2e57daa
Rudolfosehnem <rudolfo_silva@estudante.sesisenai.org.br>
2025-10-09 10:10:49 -0300

    Conexaão com rede Wi Fi

--- placa_1/placa_1.ino
+++ placa_1/placa_1.ino
@@ -1,5 +1,17 @@
+#include <WiFi.h>
+
+const String SSID = "nome";
+const String PASS = "senha";
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Conectando ao WiFi"); //apresenta essa msg na tela
+  WiFi.begin("SSID,PASS");  //tenta conectar na rede 
+  while(WiFi.status() != WL_CONNECTED                                                                                               ){
+    Serial.print("."); //Mostra "......."
+    delay(200);
+  }
+  Serial.println("\nConectado com Sucesso!")
 
 }
 
