90ae5f4e423984dd8ae4a206b9c20da52875f986
pierrewelter <pierre_welter@estudante.sesisenai.org.br>
2025-10-09 10:08:02 -0300

    Conexão com rede WiFi

--- NosTrem/NosTrem.ino
+++ NosTrem/NosTrem.ino
@@ -1,6 +1,18 @@
-void setup() {
-  //void    a put your setup code here, to run once:
+#include <WiFi.h>
 
+const String SSID  = "FIESC_IOT_EDU";
+const String PASS = "8120gv08";
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Conecando ao Wifi"); //Apresenta essa msg na tela
+  WiFi.begin(SSID,PASS); // Tenta conectar na rede
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print("."); //mostra "......."
+    delay(200);
+  }
+  Serial.println("\nConectado Com Sucesso!");
 }
 
 void loop() {
