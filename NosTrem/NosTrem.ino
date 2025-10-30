b085092c66d815586cd1eb37c5d29961dec30ff5
pierrewelter <pierre_welter@estudante.sesisenai.org.br>
2025-10-30 08:13:51 -0300

    envio de mensagens para o broker

--- NosTrem/NosTrem.ino
+++ NosTrem/NosTrem.ino
@@ -1,8 +1,14 @@
 #include <WiFi.h>
+#include <PubSubClient.h>
+
+WiFiClient client;
+PubSubClient mqtt(client);
+
+const String BrokerURL = "test.mosquitto.org";
+const int BrokerPort = 1883;
 
 const String SSID  = "FIESC_IOT_EDU";
 const String PASS = "8120gv08";
-
 
 void setup() {
   Serial.begin(115200);
@@ -13,9 +19,24 @@ void setup() {
     delay(200);
   }
   Serial.println("\nConectado Com Sucesso!");
+
+  Serial.println("Conectado ao Broker...")
+  mqtt.setServer(BrokerURL.c_str(),BrokerPort);
+  String BoardID = "NosTrem"
+  BoardID += String(random(0xffff),HEX);
+  mqtt.connect(BoardID.c_str(),BrokerURL.c_str(),BrokerPort.c_str());
+  while(!mqtt.connected()){
+  Serial.print(".");
+  delay(200);
+  }
+  Serial.println("\nConectado ao Broker!");
 }
 
 void loop() {
   //void aa bb put your main code here, to run repeatedly:
-
+String mensagem = "Nome: ";
+mensagem += "oi";
+mqtt.publish("topico-DSM14" , mensagem.c_str());
+mqtt.loop();
+delay(1000)
 }
