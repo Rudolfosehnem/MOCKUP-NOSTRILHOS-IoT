f7fda53654c6950fd58ae4f16ad0be0a9e305de8
SuperChoque123 <enzo_r_siqueira@estudante.sesisenai.org.br>
2025-11-27 08:57:54 -0300

    Update Trem.ino

--- Trem/Trem.ino
+++ Trem/Trem.ino
@@ -2,14 +2,16 @@
 #include <PubSubClient.h>
 #include "env.h"
 
+//criaçaomqttewifi
 WiFiClient client;
 PubSubClient mqtt(client);
 
-
-
+//roda tudo quando liga o ESP32
 void setup() {
   Serial.begin(115200);
   Serial.println("Conectando no WiFi");
+
+  //wificonex
   WiFi.begin(WIFI_SSID,WIFI_PASS);
   while(WiFi.status() != WL_CONNECTED){
     Serial.println(".");
@@ -17,34 +19,46 @@ void setup() {
   }
   Serial.println("\nConectado com Sucesso no WiFi!");
 
+  //brokerconfig
   Serial.println("Conectando ao Broker...");
   mqtt.setServer(BROKER_URL,BROKER_PORT);
+
+  //idaleatorio
   String BoardID = "Trem";
   BoardID += String(random(0xffff),HEX);
+
+  //conexaobroker
   mqtt.connect(BoardID.c_str() , BROKER_USER , BROKER_PASS);
   while(!mqtt.connected()){
     Serial.print(".");
     delay(200);
   }  
   mqtt.subscribe(TOPIC_ILUM);
+
+  //funçaocallback
   mqtt.setCallback(callback); // Recebe a mensagem
   Serial.println("\nConectado ao Broker!");
 }
 
+
 void loop() {
-  mqtt.publish(TOPIC_ILUM , "Acender"); // Envia a mensagem
+  mqtt.publish(TOPIC_ILUM , "Acender"); //mqttpublicaçao
   mqtt.loop();
   delay(1000);
 }
 
+//montamsg
 void callback(char* topic, type* payload, unsigned int length){
   String msg = "";
   for(int i = 0; i < length; i++){
     msg += (char) payload(1);
   }
-  if(topic=TOPIC_ILUM && msg = "Acender"){ // Verifica mensagem do tópico de luz do S1 e fala para acender led
+
+  //acende e apaga led
+  if(topic=TOPIC_ILUM && msg = "Acender"){
     digitalWrite(2,HIGH);
-  } else if (topic=TOPIC_ILUM && msg = "Apagar"){ // Verifica mensagem do tópico de luz do S1 e fala para apagar led
+    
+  } else if (topic=TOPIC_ILUM && msg = "Apagar"){
     digitalWrite(2,LOW);
   }
 }
