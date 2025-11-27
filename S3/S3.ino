353983c8c0c645f8cc6139e1fe61e98fc532eddf
SuperChoque123 <enzo_r_siqueira@estudante.sesisenai.org.br>
2025-11-27 08:48:42 -0300

    Update S3.ino

--- S3/S3.ino
+++ S3/S3.ino
@@ -9,7 +9,7 @@ PubSubClient mqtt(client);
 
 
 
-
+//ultrassônico
 const byte SERVO_PIN = 18;
 Servo Servo1;
 
@@ -24,26 +24,30 @@ const byte ECHO_PIN = 26;
 void setup() {
   Serial.begin(115200);
 
-
+//config servo
   Servo1.attach(SERVO_PIN);
   Servo2.attach(SERVO_PIN2);
 
+  //config ultrassonico
   pinMode(TRIGGER_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
 
-
+//wificonex
   client.setInsecure();
   Serial.println("Conectando ao WiFi");
   WiFi.begin(WIFI_SSID, WIFI_PASS);
-
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(200);
   }
   Serial.print("\nConectado com Sucesso!");
   Serial.println("Conectando ao Broker...");
+
+  //mqttconfig
   mqtt.setServer(BROKER_URL, BROKER_PORT);
   mqtt.setCallback(callback);
+
+  //idaleatorioESP32
   String BoardID = "S2";
   BoardID += String(random(0xffff, HEX));
   mqtt.connect(BoardID.c_str(), BROKER_USER, BROKER_PASS);
@@ -51,16 +55,16 @@ void setup() {
     Serial.println(".");
     delay(200);
   }
-  
   Serial.println("\nConectado ao Broker!");
+
+  //mqtttopicos
   mqtt.subscribe(TOPIC_ILUM);
   mqtt.subscribe(TOPIC_PRESENCA1);
   mqtt.subscribe(TOPIC_PRESENCA2);
   mqtt.subscribe(TOPIC_PRESENCA3);
 }
 
-
-
+//funcaolerdistancia
 long lerDistancia() {
   digitalWrite(TRIGGER_PIN, LOW);
   delayMicroseconds(2);
@@ -68,14 +72,13 @@ long lerDistancia() {
   delayMicroseconds(10);
   digitalWrite(TRIGGER_PIN, LOW);
 
+  //comecamediçao
   long duracao = pulseIn(ECHO_PIN, HIGH);
   long distancia = duracao * 349.24 / 2 / 10000;
-
   return distancia;
 }
 
-
-
+//loopprincipal
 void loop() {
 
   mqtt.loop();
@@ -86,6 +89,7 @@ void loop() {
   Serial.print(distancia);
   Serial.println(" cm");
 
+  //mostraseobjetoperto
   if (distancia < 10) {
     Serial.println("Objeto próximo!");
     mqtt.publish(TOPIC_DISTANCIA, "Objeto próximo!");
@@ -95,6 +99,7 @@ void loop() {
 
 }
 
+//montamsg
 void callback(char* topic, byte* payload, unsigned int length) {
   String msg = "";
   for (int i = 0; i < length; i++) {
@@ -104,19 +109,26 @@ void callback(char* topic, byte* payload, unsigned int length) {
 Serial.print("TOPICO: "); Serial.println(topic);
   Serial.print("MSG: "); Serial.println(msg);
 
+  //controleluz
   if (strcmp(topic, TOPIC_ILUM) == 0) {
     if (msg == "ligar") digitalWrite(2, HIGH);
     if (msg == "desligar") digitalWrite(2, LOW);
+
+  //servo1mov
   }else if (strcmp(topic, TOPIC_PRESENCA1) == 0) {
     if (msg == "servo1") {
       Servo1.write(0);
       Serial.println("Servo1 → posição 1 (0°)");
     }
+
+    //servo2mov
   }else if (strcmp(topic, TOPIC_PRESENCA2) == 0) {
     if (msg == "servo2") {
       Servo2.write(0);
       Serial.println("Servo2 → posição 1 (0°)");
     }
+
+    //abrirservos
   }else if (strcmp(topic, TOPIC_PRESENCA3) == 0) {
     if (msg == "abrir") {
       Servo1.write(90);
